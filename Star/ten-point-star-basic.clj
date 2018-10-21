dc3ad28af8c9e45b0621f0144b8f2978693f1090
skalster <skalster@users.noreply.github.com>
2018-10-21 20:48:08 +0200

    Add files via upload
    Add ten point star example.

---
+++ Star/ten-point-star-basic.clj
@@ -0,0 +1,17 @@
+;;ten-point-star
+(reset)
+(def steps 50)
+(def angle90 90)
+(def angle54 54)
+
+
+(defn tenPointStar [] 
+  (forward steps)
+  (left angle90)
+  (forward steps)
+  (right angle54))
+
+(rep 10 
+     (tenPointStar))
+
+(hide)
---
+++ Star/ten-point-star-color.clj
@@ -0,0 +1,20 @@
+;;ten-point-star
+(reset)
+(def steps 50)
+(def angle90 90)
+(def angle54 54)
+(set-fill "red")
+  
+  
+(defn tenPointStar [] 
+  (forward steps)
+  (left angle90)
+  (forward steps)
+  (right angle54))
+
+(filled
+  (rep 10 
+     (tenPointStar)))
+
+
+(hide)
